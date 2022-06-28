c923a3e12fa02e6ea0eaffc0f88da1b102770ad2
RoVoth <droxer@hotmail.es>
2022-06-28 17:31:54 +0200

    iteracion 1

--- js/index.js
+++ js/index.js
@@ -1,7 +1,11 @@
+console.log("hola");
 // Iteration 1: Names and Input
+let hacker1 = "Paco";
+console.log(`The drive's name is ${hacker1}`);
 
+let hacker2 = "Robert";
+console.log(`The navigator'sgit`);
 
 // Iteration 2: Conditionals
-
 
 // Iteration 3: Loops
